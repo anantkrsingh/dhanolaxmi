652ca04e10ce6094332fd6081cf402c4f7ea4f91
anantkrsingh <anantkrplay@gmail.com>
2023-08-27 21:16:03 +0530

    routes_changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = true;
-$route['backend'] = 'backend/Auth';
+$route['login'] = 'backend/Auth';
 
 $route['index'] = 'home';
 $route['download'] = 'home/download';
